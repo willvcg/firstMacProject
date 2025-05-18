f117e73029fa0fd2a2c01b02eb0a319af396d422
willvcg <91598392+willvcg@users.noreply.github.com>
2025-05-19 00:10:55 +0200

    fix: add dialog prime ng component (#8)

---
+++ src/app/components/dialog-prime-ng/dialog-prime-ng.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-prime-ng',
+  imports: [],
+  templateUrl: './dialog-prime-ng.component.html',
+  styleUrl: './dialog-prime-ng.component.scss',
+})
+export class DialogPrimeNgComponent {}
